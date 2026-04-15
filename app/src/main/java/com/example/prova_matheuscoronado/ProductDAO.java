f238125155b79189d59c56c5c9feea3a846636b0
Matheus Coronado <matheuscoronado48@gmail.com>
2026-04-15 19:45:32 -0300

    Adicionado consulta SQL para listar todos os produtos no ProductDAO

--- app/src/main/java/com/example/prova_matheuscoronado/ProductDAO.java
+++ app/src/main/java/com/example/prova_matheuscoronado/ProductDAO.java
@@ -2,10 +2,16 @@ package com.example.prova_matheuscoronado;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
+import java.util.List;
 
 @Dao
 public interface ProductDAO {
     // Método para inserir um novo produto no estoque
     @Insert
     void inserir(Product produto);
+
+    // Busca todos os produtos na tabela 'produtos' definida na Entidade
+    @Query("SELECT * FROM produtos")
+    List<Product> listarTodos();
 }
