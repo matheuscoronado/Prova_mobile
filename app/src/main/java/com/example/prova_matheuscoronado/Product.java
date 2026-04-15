8327e1b35163393d7d39c794b03bcd3d1cc4bfe1
Matheus Coronado <matheuscoronado48@gmail.com>
2026-04-15 19:39:56 -0300

    Criado metodos getters e setters para a entidade Product

--- app/src/main/java/com/example/prova_matheuscoronado/Product.java
+++ app/src/main/java/com/example/prova_matheuscoronado/Product.java
@@ -21,4 +21,20 @@ public class Product {
         this.preco = preco;
         this.quantidade = quantidade;
     }
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCodigo() { return codigo; }
+    public void setCodigo(String codigo) { this.codigo = codigo; }
+
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco; }
+
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
 }
