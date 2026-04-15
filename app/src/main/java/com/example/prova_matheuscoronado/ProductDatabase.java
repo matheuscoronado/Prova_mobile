11eca582c55dcfca38fe229d4b5afb5ce08f3892
Matheus Coronado <matheuscoronado48@gmail.com>
2026-04-15 19:48:55 -0300

    Configurado classe abstrata ProductDatabase e declarada a entidade

--- app/src/main/java/com/example/prova_matheuscoronado/ProductDatabase.java
+++ app/src/main/java/com/example/prova_matheuscoronado/ProductDatabase.java
@@ -1,4 +1,18 @@
 package com.example.prova_matheuscoronado;
 
-public class ProductDatabase {
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+// Definimos a entidade Product e a versão do banco
+@Database(entities = {Product.class}, version = 1)
+public abstract class ProductDatabase extends RoomDatabase {
+
+    // Método abstrato para acessar o DAO de produtos
+    public abstract ProductDAO productDao();
+
+    // Instância única para o padrão Singleton
+    private static ProductDatabase instancia;
+
 }
