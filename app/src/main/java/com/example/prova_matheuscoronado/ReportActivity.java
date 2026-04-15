3d9824a700141da68298e5b536b74ddbee2a9f7b
Matheus Coronado <matheuscoronado48@gmail.com>
2026-04-15 20:10:37 -0300

    configurar UI básica e navegação de retorno na ReportActivity

--- app/src/main/java/com/example/prova_matheuscoronado/ReportActivity.java
+++ app/src/main/java/com/example/prova_matheuscoronado/ReportActivity.java
@@ -1,4 +1,28 @@
 package com.example.prova_matheuscoronado;
 
-public class ReportActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.List;
+
+public class ReportActivity extends AppCompatActivity {
+    private TextView textViewRelatorio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+        textViewRelatorio = findViewById(R.id.textViewReport);
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(v -> voltarParaCadastro());
+    }
+
+    public void voltarParaCadastro() {
+        startActivity(new Intent(ReportActivity.this, MainActivity.class));
+        finish();
+    }
 }
