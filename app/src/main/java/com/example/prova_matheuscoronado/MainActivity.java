7d317fc4ed7dbabfc1b5cc9d0b381a3fce7a4e76
Matheus Coronado <matheuscoronado48@gmail.com>
2026-04-15 20:17:01 -0300

    inicializar componentes de UI e banco de dados na MainActivity

--- app/src/main/java/com/example/prova_matheuscoronado/MainActivity.java
+++ app/src/main/java/com/example/prova_matheuscoronado/MainActivity.java
@@ -1,24 +1,30 @@
 package com.example.prova_matheuscoronado;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    // Declaração dos campos de entrada e do DAO
+    private EditText editNome, editCodigo, editPreco, editQtd;
+    private ProductDAO produtoDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Mapeamento dos componentes do XML
+        editNome = findViewById(R.id.editTextProductName);
+        editCodigo = findViewById(R.id.editTextProductCode);
+        editPreco = findViewById(R.id.editTextProductPrice);
+        editQtd = findViewById(R.id.editTextProductQuantity);
+
+        // Inicialização do Banco de Dados Room
+        ProductDatabase db = ProductDatabase.getInstance(this);
+        produtoDao = db.productDao();
     }
 }
