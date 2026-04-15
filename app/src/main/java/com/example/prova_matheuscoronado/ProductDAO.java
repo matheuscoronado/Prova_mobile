a0fb016aa0ba6b748d2d6a6c2f8d85b79be85891
Matheus Coronado <matheuscoronado48@gmail.com>
2026-04-15 19:44:14 -0300

    Definido ProductDAO e implementar método de inserção de produtos

--- app/src/main/java/com/example/prova_matheuscoronado/ProductDAO.java
+++ app/src/main/java/com/example/prova_matheuscoronado/ProductDAO.java
@@ -1,4 +1,11 @@
 package com.example.prova_matheuscoronado;
 
-public class ProductDAO {
+import androidx.room.Dao;
+import androidx.room.Insert;
+
+@Dao
+public interface ProductDAO {
+    // Método para inserir um novo produto no estoque
+    @Insert
+    void inserir(Product produto);
 }
